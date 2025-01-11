5da0e430ba59db478dad8c0fce2f07a54017f1c5
owusuvictorjnr <owusuvictor80@gmail.com>
2025-01-11 09:07:39 +0000

    constants

---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "best-buy";
+export const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || "...the best place to buy";
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  "The best place to buy is Best Buy, where you can find everything you need at a great price. Shop with us today and get what you need for your home or office.";
