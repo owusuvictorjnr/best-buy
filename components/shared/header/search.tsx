6b956c2910c683f563be4976770ff86a5e4499cc
owusuvictorjnr <owusuvictor80@gmail.com>
2025-01-11 09:21:58 +0000

    refactor input and button classname

--- components/shared/header/search.tsx
+++ components/shared/header/search.tsx
@@ -10,7 +10,7 @@ import { APP_NAME, APP_SLOGAN } from "@/lib/constants";
 import { SearchIcon } from "lucide-react";
 import React from "react";
 
-const categories = [ "men", "women", "kids", "accessories"];
+const categories = ["men", "women", "kids", "accessories"];
 export default async function Search() {
   return (
     <form action="/search" method="GET" className="flex items-stretch h-10">
@@ -32,12 +32,12 @@ export default async function Search() {
         type="search"
         name="q"
         placeholder={`${APP_NAME}, ${APP_SLOGAN}`}
-        className="w-full h-full rounded-none rounded-l-none border-l-0"
+        className="h-full rounded-none flex-1 dark:border-gray-200 bg-gray-100 text-black text-base"
       />
 
       <button
         type="submit"
-        className="bg-primary text-primary-foreground text-black rounded-s-none rounded-e-md h-full px-3 py-3"
+        className="bg-primary text-primary-foreground text-black rounded-s-none rounded-e-md h-full px-3 py-2"
       >
         <SearchIcon className="w-6 h-6" />
       </button>
