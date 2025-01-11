9930e78b5a20997caf5b9be0e02f4adabeffc937
owusuvictorjnr <owusuvictor80@gmail.com>
2025-01-11 09:01:34 +0000

    home layout

---
+++ app/(home)/layout.tsx
@@ -0,0 +1,12 @@
+import { Footer } from "@/components/shared/footer";
+import Header from "@/components/shared/header";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-1 flex flex-col">{children}</main>
+      <Footer />
+    </div>
+  );
+}
