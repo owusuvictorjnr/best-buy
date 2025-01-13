6fef3adf34776c45bc00189117a31e3514bca5da
owusuvictorjnr <owusuvictor80@gmail.com>
2025-01-13 00:18:42 +0000

    implement currency formatter

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,14 +5,39 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+// This function formats a number to always display two decimal places.
+// If the input number has less than two decimal places, it pads the
+// remaining places with zeros
 export const formatNumberWithDecimal = (num: number): string => {
   const [int, decimal] = num.toString().split(".");
   return decimal ? `${int}.${decimal.padEnd(2, "0")}` : int;
 };
 
+// This function converts a string to a URL-friendly slug by:
+// 1. Removing all non-alphanumeric characters
+// 2. Replacing spaces with hyphens
+// 3. Removing leading and trailing hyphens
 export const toSlug = (text: string): string =>
   text
     .toLowerCase()
     .replace(/[^\w\s-]+/g, "")
     .replace(/\s+/g, "-")
     .replace(/^-+|-+$/g, "");
+
+const CURRENCY_FORMATTER = new Intl.NumberFormat("en-US", {
+  currency: "USD",
+  style: "currency",
+  minimumFractionDigits: 2,
+});
+
+// This function formats a given number as a US dollar currency string,
+// always displaying two decimal places (e.g., $12.34
+export function formatCurrency(amount: number) {
+  return CURRENCY_FORMATTER.format(amount);
+}
+
+const NUMBER_FORMATTER = new Intl.NumberFormat("en-US");
+
+export function formatNumber(number: number) {
+  return NUMBER_FORMATTER.format(number);
+}
