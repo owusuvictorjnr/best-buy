3112c76443919f8addf671ae1c269deeec78b5dd
owusuvictorjnr <owusuvictor80@gmail.com>
2025-01-15 19:06:11 +0000

    order-item and cart type

--- types/index.ts
+++ types/index.ts
@@ -1,4 +1,8 @@
-import { ProductInputSchema } from "@/lib/validator";
+import {
+  OrderItemSchema,
+  ProductInputSchema,
+  CartSchema,
+} from "@/lib/validator";
 import { z } from "zod";
 
 export type IProductInput = z.infer<typeof ProductInputSchema>;
@@ -18,3 +22,7 @@ export type Data = {
     isPublished: boolean;
   }[];
 };
+
+export type OrderItem = z.infer<typeof OrderItemSchema>;
+
+export type Cart = z.infer<typeof CartSchema>;
