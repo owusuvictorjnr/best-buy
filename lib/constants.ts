6ea8979c2345b74bf82056c29584033c739c2f58
owusuvictorjnr <owusuvictor80@gmail.com>
2025-01-15 19:03:26 +0000

    free-shipping-min-price

--- lib/constants.ts
+++ lib/constants.ts
@@ -7,3 +7,6 @@ export const APP_DESCRIPTION =
 
 export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9);
 
+export const FREE_SHIPPING_MIN_PRICE = Number(
+  process.env.FREE_SHIPPING_MIN_PRICE || 35
+);
